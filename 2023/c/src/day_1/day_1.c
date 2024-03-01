3b43d797a93d80e454a0e0b1020748b0401ba869
Brandon Catubig <bcatubig@pm.me>
2024-02-29 23:24:24 -0800

    implement linked-list

--- 2023/c/src/day_1/day_1.c
+++ 2023/c/src/day_1/day_1.c
@@ -1,31 +1,116 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return EXIT_FAILURE;
-    };
+#include <string.h>
+#include "day_1.h"
 
-    printf("loading: %s\n", argv[1]);
-    FILE* in_file = fopen(argv[1], "r");
-    if (in_file == NULL) {
-        perror("fopen");
-        return EXIT_FAILURE;
-    };
+void list_init(struct List *list)
+{
+	memset(list, 0, sizeof(struct List));
+	list->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+}
 
-    char* buf = NULL;
-    size_t buf_size = 0;
-    ssize_t nread;
+void list_destroy(struct List *list)
+{
+	while (list_size(list) > 0) {
+		list_rem_next(list, NULL);
+	}
+	memset(list, 0, sizeof(struct List));
+}
 
-    while ((nread = getline(&buf, &buf_size, in_file)) != -1) {
-        printf("%s", buf);
-        for (int i = 0; i < nread - 1; i++) {
-            printf("%c - isNum: %d\n", buf[i], isdigit(buf[i]) != 0);
-        }
-    }
+int list_ins_next(struct List *list, struct ListElement *element, int data)
+{
+	struct ListElement *new_element;
 
-    free(buf);
-    fclose(in_file);
-    return EXIT_SUCCESS;
+	new_element = (struct ListElement *)malloc(sizeof(struct ListElement));
+
+	if (new_element == NULL) {
+		return -1;
+	}
+
+	new_element->data = data;
+
+	// Handle insertion at head of list
+	if (element == NULL) {
+		if (list_size(list) == 0) {
+			list->tail = new_element;
+		}
+
+		new_element->next = list->head;
+		list->head = new_element;
+	} else {
+		if (element->next == NULL) {
+			list->tail = new_element;
+		}
+
+		new_element->next = element->next;
+		element->next = new_element;
+	}
+
+	list->size++;
+
+	return 0;
+}
+
+int list_rem_next(struct List *list, struct ListElement *element)
+{
+	struct ListElement *old_element;
+
+	if (list_size(list) == 0) {
+		return -1;
+	}
+
+	// remove the head
+	if (element == NULL) {
+		old_element = list->head;
+		list->head = list->head->next;
+
+		if (list_size(list) == 1) {
+			list->tail = NULL;
+		}
+	} else {
+		if (element->next == NULL) {
+			return -1;
+		}
+
+		old_element = element->next;
+		if (element->next == NULL) {
+			list->tail = element;
+		}
+	}
+
+	free(old_element);
+
+	list->size--;
+
+	return 0;
+}
+
+int read_calibration_value(FILE *f)
+{
+	// thing
+	return 0;
+}
+
+int read_line_value(char *line, size_t line_length)
+{
+	struct List *nums;
+
+	nums = (struct List *)(sizeof(struct List));
+
+	if (nums == NULL) {
+		return -1;
+	}
+
+	for (int i = 0; i < line_length - 1; i++) {
+		if isdigit (line[i]) {
+			// add to list
+			int rv;
+		}
+		printf("%c - isNum: %d\n", line[i], isdigit(line[i]) != 0);
+	}
+
+	return 0;
 }
--- 2023/c/src/day_1/day_1.h
+++ 2023/c/src/day_1/day_1.h
@@ -1,6 +1,44 @@
 #ifndef DAY_1_H
 #define DAY_1_H
 
-#define FOO 1
+#include <stdio.h>
+
+struct ListElement {
+	int data;
+	struct ListElement *next;
+};
+
+struct List {
+	int size;
+	struct ListElement *head;
+	struct ListElement *tail;
+};
+
+void list_init(struct List *list);
+
+void list_destroy(struct List *list);
+
+int (*match)(const void *key1, const void *key2);
+
+int list_ins_next(struct List *list, struct ListElement *element, int data);
+int list_rem_next(struct List *list, struct ListElement *element);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+#define list_is_tail(element) ((element)->next == NULL ? 1 : 0)
+#define list_next(element) ((element)->next)
+#define list_data(element) ((element)->data)
+
+/*
+* Function: read_calibration_value
+* Computes the calibration value for launching a trebuchet
+*
+* f: the calibration document
+*
+* returns: the calibration value. -1 if an error occured
+*/
+int read_calibration_value(FILE *f);
 
 #endif
